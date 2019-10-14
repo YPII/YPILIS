21bb0a0a174005d75e3d7baa7b6f6ab3d69116ce
WilliamCopland <william.copland@ypii.com>
2019-10-14 12:02:14 -0600

    Add AuthorizationForVerbalTestRequest Test, test Order, Word Doc, result page andpath

---
+++ YellowstonePathology/Business/Test/AuthorizationForVerbalTestRequest/AuthorizationForVerbalTestRequestTestOrder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YellowstonePathology.Business.Persistence;
+
+namespace YellowstonePathology.Business.Test.AuthorizationForVerbalTestRequest
+{
+    [PersistentClass("tblAuthorizationForVerbalTestRequestTestOrder", "tblPanelSetOrder", "YPIDATA")]
+    public class AuthorizationForVerbalTestRequestTestOrder : YellowstonePathology.Business.Test.PanelSetOrder
+    {
+        private string m_Comment;
+
+        public AuthorizationForVerbalTestRequestTestOrder()
+        { }
+
+        public AuthorizationForVerbalTestRequestTestOrder(string masterAccessionNo, string reportNo, string objectId,
+            YellowstonePathology.Business.PanelSet.Model.PanelSet panelSet,
+            YellowstonePathology.Business.Interface.IOrderTarget orderTarget,
+            bool distribute)
+			: base(masterAccessionNo, reportNo, objectId, panelSet, orderTarget, distribute)
+        { }
+
+        [PersistentProperty()]
+        public string Comment
+        {
+            get { return this.m_Comment; }
+            set
+            {
+                if (this.m_Comment != value)
+                {
+                    this.m_Comment = value;
+                    this.NotifyPropertyChanged("Comment");
+                }
+            }
+        }
+
+        public override string ToResultString(Business.Test.AccessionOrder accessionOrder)
+        {
+            string result = "Comment: " + this.Comment;
+            return result;
+        }
+    }
+}
---
+++ YellowstonePathology/Business/Test/AuthorizationForVerbalTestRequest/AuthorizationForVerbalTestRequestWordDocument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.AuthorizationForVerbalTestRequest
+{
+    public class AuthorizationForVerbalTestRequestWordDocument : YellowstonePathology.Business.Document.CaseReportV2
+	{
+        public AuthorizationForVerbalTestRequestWordDocument(Business.Test.AccessionOrder accessionOrder, Business.Test.PanelSetOrder panelSetOrder, YellowstonePathology.Business.Document.ReportSaveModeEnum reportSaveMode) 
+            : base(accessionOrder, panelSetOrder, reportSaveMode)
+        {
+
+        }
+
+        public override void Render()
+        {
+            this.m_TemplateName = @"\\CFileServer\Documents\ReportTemplates\XmlTemplates\AuthorizationForVerbalTestRequest.xml";
+            base.OpenTemplate();
+
+            base.SetDemographicsV2();
+
+            YellowstonePathology.Business.Amendment.Model.AmendmentCollection amendmentCollection = this.m_AccessionOrder.AmendmentCollection.GetAmendmentsForReport(m_PanelSetOrder.ReportNo);
+            YellowstonePathology.Business.Document.AmendmentSection amendmentSection = new YellowstonePathology.Business.Document.AmendmentSection();
+            amendmentSection.SetAmendment(amendmentCollection, this.m_ReportXml, this.m_NameSpaceManager, false);
+
+            AuthorizationForVerbalTestRequestTestOrder panelSetOrder = (AuthorizationForVerbalTestRequestTestOrder)this.m_PanelSetOrder;
+
+            if (string.IsNullOrEmpty(panelSetOrder.Comment) == false) base.ReplaceText("report_comment", panelSetOrder.Comment);
+            else base.DeleteRow("report_comment");
+
+            this.SetReportDistribution();
+            this.SetCaseHistory();
+
+            this.SaveReport();
+        }
+    }
+}
