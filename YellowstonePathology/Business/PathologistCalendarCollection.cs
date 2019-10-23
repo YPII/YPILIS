daee4c253effc1bc21dc94e90c406468a284f5c8
sidharder <sideharder@gmail.com>
2019-10-23 10:14:34 -0600

    pathology calendar fixex

--- YellowstonePathology/Business/PathologistCalendarCollection.cs
+++ YellowstonePathology/Business/PathologistCalendarCollection.cs
@@ -1,35 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-<<<<<<< HEAD
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-
-namespace YellowstonePathology.Business
-{
-    public class PathologistCalendarCollection
-    {
-        public PathologistCalendarCollection()
-        {
-            
-        }
-
-        public void InsertThisMonth()
-        {
-            PathologyCalendar pathologistCalendar = new PathologyCalendar();
-        }
-
-        public void Insert()
-        {
-            //MySqlCommand cmd = new MySqlCommand("Select JSONValue from tblHoliday where HolidayDate between @StartDate and @EndDate order by HolidayDate;");
-            //cmd.CommandType = System.Data.CommandType.Text;
-            //cmd.Parameters.AddWithValue("@StartDate", startDate);            
-        }
-
-    }    
-=======
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Data;
 using MySql.Data.MySqlClient;
@@ -119,5 +90,4 @@ namespace YellowstonePathology.Business
             return tmp.Count > 0 ? false : true;
         }
     }
->>>>>>> 92a9298d26283fb9b336860bd807920c31fc886f
 }
--- YellowstonePathology/UI/PathologistCalendarDialog.xaml.cs
+++ YellowstonePathology/UI/PathologistCalendarDialog.xaml.cs
@@ -15,25 +15,6 @@ using System.Windows.Shapes;
 namespace YellowstonePathology.UI
 {
     /// <summary>
-<<<<<<< HEAD
-    /// Interaction logic for PathologistCalendarDialog.xaml
-    /// </summary>
-    public partial class PathologistCalendarDialog : Window
-    {
-        private Business.PathologistCalendarCollection m_PathologistCalendarCollection;
-
-        public PathologistCalendarDialog()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-        }
-
-        
-
-        public Business.PathologistCalendarCollection PathologistCalendarCollection
-        {
-            get { return this.m_PathologistCalendarCollection; }            
-=======
     /// Interaction logic for PathologistCalendar.xaml
     /// </summary>
     public partial class PathologistCalendarDialog : Window
@@ -75,7 +56,6 @@ namespace YellowstonePathology.UI
         private void ButtonGetCalendar_Click(object sender, RoutedEventArgs e)
         {
             this.m_PathologistCalendarCollection = YellowstonePathology.Business.PathologistCalendarCollection.Load(this.m_StartDate, this.m_StartDate.AddMonths(1).AddDays(-1));
->>>>>>> 92a9298d26283fb9b336860bd807920c31fc886f
         }
     }
 }
