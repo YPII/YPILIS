e0d1525f755cec584c560076206e9dc977fdbd2b
WilliamCopland <william.copland@ypii.com>
2019-10-30 09:30:07 -0600

    Add new class

---
+++ YellowstonePathology/Business/Calendar/PathologistsByLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Calendar
+{
+    public class PathologistsByLocation
+    {
+        private int m_TotalCount;
+        private int m_BillingsCount;
+        private int m_BozemanCount;
+        public PathologistsByLocation()
+        { }
+    }
+}
