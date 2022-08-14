3fe52ed57c362187acc0fbde72fafd8ebeffd8e0
sidharder <sideharder@gmail.com>
2022-08-14 17:22:05 -0600

    stuff

---
+++ YellowstonePathology/Business/ClientOrder.Model/UniversalServiceCollectionNMH.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.ClientOrder.Model
+{
+    public class UniversalServiceCollectionNMH : ObservableCollection<UniversalService>
+    {
+        public UniversalServiceCollectionNMH()
+        {
+            this.Add(new UniversalService("NG", "Cytology Non-Gyn Path Specimen", UniversalServiceApplicationNameEnum.NMH, 13));
+            this.Add(new UniversalService("PAP", "Pap Smear", UniversalServiceApplicationNameEnum.NMH, 15));
+            //this.Add(new UniversalService("THINPREP", "Thin Prep Pap", UniversalServiceApplicationNameEnum.ECLINICALWORKS, 15));
+            //this.Add(new UniversalService("THINPREP", "Thin Prep Pap", UniversalServiceApplicationNameEnum.ECLINICALWORKS, 116));
+            //this.Add(new UniversalService("NGCT", "Chlamydia Gonorrhea Screening", UniversalServiceApplicationNameEnum.ECLINICALWORKS, 3));
+            //this.Add(new UniversalService("HRHPVTEST", "High Risk HPV", UniversalServiceApplicationNameEnum.ECLINICALWORKS, 14));
+            //this.Add(new UniversalService("HPVG1618", "HPV Genotypes 16 and 18", UniversalServiceApplicationNameEnum.ECLINICALWORKS, 62));            
+        }
+
+        public UniversalService GetByUniversalServiceId(string universalServiceId)
+        {
+            UniversalService result = null;
+            foreach (UniversalService us in this)
+            {
+                if (us.UniversalServiceId == universalServiceId)
+                {
+                    result = us;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public UniversalService GetByPanelSetId(int panelSetId)
+        {
+            UniversalService result = null;            
+            foreach(UniversalService us in this)
+            {
+                if(us.PanelSetId == panelSetId)
+                {
+                    result = us;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ YellowstonePathology/UI/ReferenceLabTestingWorkspace.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YellowstonePathology.UI
+{
+    /// <summary>
+    /// Interaction logic for ReferenceLabTestingWorkspace.xaml
+    /// </summary>
+    public partial class ReferenceLabTestingWorkspace : UserControl
+    {
+        private string m_DocumentPath = @"\\ypiiinterface2\ChannelData\Incoming\Neogenomics";
+        private List<System.IO.FileInfo> m_FileInfoList;
+
+        public ReferenceLabTestingWorkspace()
+        {
+            this.m_FileInfoList = new List<System.IO.FileInfo>();
+            string[] files = System.IO.Directory.GetFiles(this.m_DocumentPath);
+            foreach (string file in files)
+                this.m_FileInfoList.Add(new System.IO.FileInfo(file));
+            InitializeComponent();
+            this.DataContext = this;                   
+        }
+
+        public List<System.IO.FileInfo> FileInfoList
+        {
+            get { return this.m_FileInfoList; }
+        }
+    }
+}
